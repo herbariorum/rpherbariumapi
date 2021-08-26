bdf11583718a2129c50141c0c0908893b0b06020
herbariorum <eliaspbareia@gmail.com>
2021-08-26 16:09:45 -0400

    Fisrt Commit

---
+++ src/main/java/com/digitalinnovation/herbariorumapi/HerbariorumapiApplication.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.herbariorumapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HerbariorumapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HerbariorumapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/digitalinnovation/herbariorumapi/HerbariorumapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalinnovation.herbariorumapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HerbariorumapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

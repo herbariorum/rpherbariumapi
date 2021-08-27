4ed2948f28cf2f573eea082bd83d18f3677826a9
eliaspbareia@gmail.com <eliaspbareia@gmail.com>
2021-08-27 16:24:54 -0400

    Atualização do controle e service

--- src/main/java/com/digitalinnovation/herbariorumapi/controller/PersonController.java
+++ src/main/java/com/digitalinnovation/herbariorumapi/controller/PersonController.java
@@ -15,15 +15,11 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonController {
-
 
     private PersonService personService;
 
-    @Autowired
-    public PersonController(PersonService personService) {
-        this.personService = personService;
-    }
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
@@ -39,6 +35,11 @@ public class PersonController {
     @GetMapping("/{id}")
     public PersonDTO findById(@PathVariable Long id) throws PersonNotFoundException {
         return personService.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public MessageResponseDTO updateById(@PathVariable Long id, @RequestBody @Valid PersonDTO personDTO) throws PersonNotFoundException {
+        return personService.updateById(id, personDTO);
     }
 
     @DeleteMapping("/{id}")
--- src/main/java/com/digitalinnovation/herbariorumapi/service/PersonService.java
+++ src/main/java/com/digitalinnovation/herbariorumapi/service/PersonService.java
@@ -7,6 +7,7 @@ import com.digitalinnovation.herbariorumapi.exception.PersonNotFoundException;
 import com.digitalinnovation.herbariorumapi.mapper.PersonMapper;
 import com.digitalinnovation.herbariorumapi.repository.PersonRepository;
 
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,26 +16,18 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
-
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class PersonService {
     // adiciona o repository
     private final PersonRepository personRepository;
-
-    @Autowired
-    public PersonService(PersonRepository personRepository) {
-        this.personRepository = personRepository;
-    }
 
     private final PersonMapper personMapper = PersonMapper.INSTANCE;
 
     public MessageResponseDTO createPerson(PersonDTO personDTO){
         Person personToSave = personMapper.toModel(personDTO);
 
-        Person savedPerson = personRepository.save(personToSave);
-        return MessageResponseDTO
-                .builder()
-                .message("Created person with ID "+savedPerson.getId())
-                .build();
+        Person saved = personRepository.save(personToSave);
+        return createMessageResponse(saved.getId(), "Created person with ID ");
     }
 
 
@@ -45,10 +38,6 @@ public class PersonService {
                 .collect(Collectors.toList());
     }
 
-    public Person verifyIfExists(Long id) throws PersonNotFoundException{
-        return personRepository.findById(id).orElseThrow(()-> new PersonNotFoundException(id));
-
-    }
 
     public PersonDTO findById(Long id) throws PersonNotFoundException {
         Person person = verifyIfExists(id);
@@ -59,4 +48,23 @@ public class PersonService {
         verifyIfExists(id);
         personRepository.deleteById(id);
     }
-}
+
+    public MessageResponseDTO updateById(Long id, PersonDTO personDTO) throws PersonNotFoundException{
+        verifyIfExists(id);
+        Person personToUpdate = personMapper.toModel(personDTO);
+
+        Person updated = personRepository.save(personToUpdate);
+        return createMessageResponse(updated.getId(), "Update person with ID ");
+    }
+
+    private Person verifyIfExists(Long id) throws PersonNotFoundException{
+        return personRepository.findById(id).orElseThrow(()-> new PersonNotFoundException(id));
+
+    }
+
+    private MessageResponseDTO createMessageResponse(Long id, String message) {
+        return MessageResponseDTO
+                .builder()
+                .message(message + id)
+                .build();
+    }}
